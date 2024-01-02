c37d934f3298fd799e7c85167bbe590a982b5238
Yana-Romanenko <yanaromanenko@ukr.net>
2024-01-02 20:04:20 +0200

    updated files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,33 +2,46 @@
 
 const feedbackForm = document.querySelector('.feedback-form');
 const STORAGE_DATA_KEY = 'feedback-form-state';
-
-feedbackForm.addEventListener('input', event => {
-    const formData = new formData(feedbackForm);
-    const formObject = {};
-    formData.forEach(value, key => {
-        formObject[key] = value.trim();   
-    });
-    localStorage.setItem(STORAGE_DATA_KEY, JSON.stringify(formObject));
-})
-
-feedbackForm.addEventListener('submit', event => {
-    event.preventDefault();
-    localStorage.removeItem(STORAGE_DATA_KEY);
-    feedbackForm.reset();
-    console.log(initialFormData);
-})
+ let formObject = {};
 
 try {
-    const initialFormData = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-    Array.from(feedbackForm.elements).forEach(element => {
-        const storageValue = initialFormData[element.name];
-        if (initialFormData) {
-            return;
-        } else {
-            element.value = storageValue;
-        }
-    });
-    } catch (element) {
-    console.error('PARSE ERROR')
+  const storageValue = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
+  if (storageValue) {
+    for (let key in formObject) {
+     console.log(feedbackForm.elements[key].value = formObject[key]); 
+    }
+  }
+} catch (error) {
+  console.log('PARSE ERROR');
 }
+
+// feedbackForm.addEventListener('input', event => {
+//    
+//     formData.forEach(value, key => {
+//         formObject[key] = value.trim();   
+//     });
+//     localStorage.setItem(STORAGE_DATA_KEY, JSON.stringify(formObject));
+// })
+
+// feedbackForm.addEventListener('submit', event => {
+//     event.preventDefault();
+//     localStorage.removeItem(STORAGE_DATA_KEY);
+//     feedbackForm.reset();
+//     console.log(storageValue);
+// });
+
+
+
+
+
+feedbackForm.addEventListener('input', event => {
+  formObject[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_DATA_KEY, JSON.stringify(formObject));
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(formObject);
+  localStorage.removeItem(STORAGE_DATA_KEY);
+  feedbackForm.reset();
+});
